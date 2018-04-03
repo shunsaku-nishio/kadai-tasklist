af6304dd0b32ed11bbe9ae7ebb5befca6689c9a7
shunsaku.nishio <ec2-user@ip-172-31-33-161.us-east-2.compute.internal>
2018-04-03 10:22:39 +0000

    kadai12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :correct_user, only: [:destroy, :update]
+  before_action :correct_user, only: [:destroy, :update, :show]
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
